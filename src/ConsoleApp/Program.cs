d12716c35534eb7c1625ddf853f7271b7d9d07a8
Victor Grigoriu <vgrigoriu@gmail.com>
2015-08-01 16:28:17 +0300

    Start emitting C# code

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using static System.Console;
 
 namespace ConsoleApp
@@ -8,8 +9,34 @@ namespace ConsoleApp
         private static void Main()
         {
             var columnsProvider = new ColumnsProvider(@"Server=.\SQLEXPRESS;Database=LearnORM;Trusted_Connection=True;");
-            var columns = columnsProvider.GetColumnsAsync("dbo", "Book").GetAwaiter().GetResult();
-            columns.ToList().ForEach(WriteLine);
+            var tableName = "Book";
+            var columns = columnsProvider.GetColumnsAsync("dbo", tableName).GetAwaiter().GetResult();
+
+            var writer = Out;
+
+            writer.Write(@"namespace Cucu
+{
+    [Migration(");
+            writer.Write(DateTime.Now.ToString("yyyyMMddHHmmss"));
+            writer.Write(@")]
+    public class Vaca : Migration
+    {
+        public override void Up()
+        {
+            Create.Table(""");
+            writer.Write(tableName);
+            writer.WriteLine(@""")");
+
+            columns.ToList().ForEach(writer.WriteLine);
+
+            writer.WriteLine(@"        }
+
+        public override void Down()
+        {
+            // nothing here yet
+        }
+    }
+}");
         }
     }
 }
