5eabb25d8e8a194b8ec2a79d557a114b215f99b6
Victor Grigoriu <vgrigoriu@gmail.com>
2015-08-01 12:34:23 +0300

    Open db connection

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace ConsoleApp
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            Work().GetAwaiter().GetResult();
+        }
+
+        private static async Task Work()
+        {
+            const string connectionString = @"Server=.\SQLEXPRESS;Database=LearnORM;Trusted_Connection=True;";
+
+            using (DbConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+            }
         }
     }
 }
