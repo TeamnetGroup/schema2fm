a1e9273936fec0046fbb79ebf6dae7b7b540970f
Victor Grigoriu <vgrigoriu@gmail.com>
2015-08-01 17:09:55 +0300

    Move class Table to its own file

---
+++ src/ConsoleApp/Table.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    public class Table
+    {
+        private readonly string tableName;
+        private readonly IEnumerable<Column> columns;
+
+        public Table(string tableName, IEnumerable<Column> columns)
+        {
+            this.tableName = tableName;
+            this.columns = columns;
+        }
+
+        public void OutputMigrationCode(TextWriter writer)
+        {
+            writer.Write(@"namespace Cucu
+{
+    [Migration(");
+            writer.Write(DateTime.Now.ToString("yyyyMMddHHmmss"));
+            writer.Write(@")]
+    public class Vaca : Migration
+    {
+        public override void Up()
+        {
+            Create.Table(""");
+            writer.Write(tableName);
+            writer.Write(@""")");
+
+            columns.ToList().ForEach(c =>
+            {
+                writer.WriteLine();
+                writer.Write(c.FluentMigratorCode());
+            });
+
+            writer.WriteLine(@";
+        }
+
+        public override void Down()
+        {
+            // nothing here yet
+        }
+    }
+}");
+        }
+    }
+}
