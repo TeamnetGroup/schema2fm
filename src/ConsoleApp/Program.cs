50501b51a389e33f9d31319fcfe60da9a3f5d1cb
Victor Grigoriu <vgrigoriu@gmail.com>
2015-08-01 13:23:12 +0300

    Read table id

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Common;
+﻿using System;
+using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
 
@@ -14,10 +16,40 @@ namespace ConsoleApp
         private static async Task Work()
         {
             const string connectionString = @"Server=.\SQLEXPRESS;Database=LearnORM;Trusted_Connection=True;";
+            const string schemaName = "dbo";
+            const string tableName = "Book";
 
             using (DbConnection connection = new SqlConnection(connectionString))
             {
                 await connection.OpenAsync();
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = @"select t.object_id from sys.tables t
+inner join sys.schemas s
+on t.schema_id = s.schema_id
+where s.name = @schemaName and t.name = @tableName";
+                    var schemaNameParameter = command.CreateParameter();
+                    schemaNameParameter.DbType = DbType.String;
+                    schemaNameParameter.ParameterName = "@schemaName";
+                    schemaNameParameter.Value = schemaName;
+                    command.Parameters.Add(schemaNameParameter);
+
+                    var tableNameParameter = command.CreateParameter();
+                    tableNameParameter.DbType = DbType.String;
+                    tableNameParameter.ParameterName = "@tableName";
+                    tableNameParameter.Value = tableName;
+                    command.Parameters.Add(tableNameParameter);
+
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            var tableId = reader.GetFieldValue<int>(0);
+                            Console.WriteLine($"Table has object id {tableId}");
+                        }
+                    }
+                }
             }
         }
     }
