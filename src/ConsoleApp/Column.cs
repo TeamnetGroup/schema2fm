9961a131315f073ffc8c2f898a5d7c1830b13011
Victor Grigoriu <vgrigoriu@gmail.com>
2015-08-01 16:12:10 +0300

    Move Column file to its own class

---
+++ src/ConsoleApp/Column.cs
@@ -0,0 +1,23 @@
+namespace ConsoleApp
+{
+    public class Column
+    {
+        private readonly string name;
+        private readonly string type;
+        private readonly bool isNullable;
+        private readonly short maxLength;
+
+        public Column(string name, string type, bool isNullable, short maxLength)
+        {
+            this.name = name;
+            this.type = type;
+            this.isNullable = isNullable;
+            this.maxLength = maxLength;
+        }
+
+        public override string ToString()
+        {
+            return $"Column {name} is {type}({maxLength}) and {isNullable}";
+        }
+    }
+}
--- src/ConsoleApp/ColumnsProvider.cs
+++ src/ConsoleApp/ColumnsProvider.cs
@@ -6,27 +6,6 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-    public class Column
-    {
-        private readonly string name;
-        private readonly string type;
-        private readonly bool isNullable;
-        private readonly short maxLength;
-
-        public Column(string name, string type, bool isNullable, short maxLength)
-        {
-            this.name = name;
-            this.type = type;
-            this.isNullable = isNullable;
-            this.maxLength = maxLength;
-        }
-
-        public override string ToString()
-        {
-            return $"Column {name} is {type}({maxLength}) and {isNullable}";
-        }
-    }
-
     public class ColumnsProvider
     {
         private readonly string connectionString;
