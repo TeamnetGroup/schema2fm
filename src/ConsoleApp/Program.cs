7d697130dee5f03eb58ca33cd8af1e8014e428f5
Victor Grigoriu <vgrigoriu@gmail.com>
2015-08-01 17:13:58 +0300

    Read parameters from command line arguments

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -4,11 +4,17 @@ namespace ConsoleApp
 {
     class Program
     {
-        private static void Main()
+        private static void Main(string[] args)
         {
-            var connectionString = @"Server=.\SQLEXPRESS;Database=LearnORM;Trusted_Connection=True;";
-            var schemaName = "dbo";
-            var tableName = "Book";
+            if (args.Length != 3)
+            {
+                WriteLine("Usage: schema2fm.exe connectionstring schema table");
+                return;
+            }
+
+            var connectionString = args[0];//@"Server=.\SQLEXPRESS;Database=LearnORM;Trusted_Connection=True;";
+            var schemaName = args[1];//"dbo";
+            var tableName = args[2];//"Book";
 
             var columnsProvider = new ColumnsProvider(connectionString);
             var columns = columnsProvider.GetColumnsAsync(schemaName, tableName).GetAwaiter().GetResult();
